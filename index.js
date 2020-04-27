98659a8e420a0857cb30cd36d615b35ac404d271
Dany <53636771+Dany-LF@users.noreply.github.com>
2020-04-27 21:20:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
-Spamming Bot ReMaked By IP#4773 
+Spamming Bot ReMaked By IP#4773(lol) 
 [+] Anti Banned
 [+] next Speed generation
 [+] Power full for Each User
@@ -7,8 +7,8 @@ Spamming Bot ReMaked By IP#4773
       HOW TO USE 
 + Get Each Token and Join to Server Target
 + Remix Or Clone this App
-+ 1 Project, 1 Token  [MUST SAME CODE AND PREFIX!!]
-+ Paste Token in .env (TOKEN=)
++ 1 Project, 1 Token  [MUST SAME CODE AND PREFIX!!] Shitty code we can just add tokens file and use one script for all...
++ Edit config file (SHIT SHIT SHIT)
 
 And Happy Spamming :)
 don't edit for keep safe
@@ -16,72 +16,46 @@ don't edit for keep safe
 Thx to https://github.com/Nuno135/
 **/
 
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log('Pinging');
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+const http = require('http'),
+express = require('express'),
+ app = express(),
+ conf = require('./config.json');
 
-var Discord = require('discord.js');
-var bot = new Discord.Client();
-var config = require('./prefix.json');
-var prefix = config.prefix;
+var Discord = require('discord.js'),
+bot = new Discord.Client(),
+prefix = conf.prefix;
+
+
+app.get("/", (request, response) => {console.log('Pinging');response.sendStatus(200);});
+app.listen(conf.port);
+setInterval(() => {http.get(`http://${conf.glitchpname}.glitch.me`);}, 280000);
+
 bot.on('message', msg => {
 var suffix = msg.content.split(' ').slice(1);
 //CMDS
 if (msg.content.startsWith(prefix + "help")) {
     var help = suffix[0];
     if (!help) {
-    msg.channel.send([
-        '```js' + 
-        '\nSPECIFIC COMMANDS:' + 
-        `\n${prefix}help **spam**` +
-        `\n${prefix}help **dspam**` + 
-        `\n${prefix}help **pmspam**` + 
-        `\n${prefix}help **dpmspam**` + 
-        `\n${prefix}help **cspam**` +
-        '```'
-    ])
+    msg.channel.send(`\`\`\`js\nSPECIFIC COMMANDS:\n\n${prefix}help **spam**\n${prefix}help **dspam**\n${prefix}help **pmspam**\n${prefix}help **dpmspam**\n${prefix}help **cspam**\`\`\`\n`)
     } else {
      if (help === "spam") {
-         msg.channel.send([
-             '```js\n Mau Spam Apa!.' + 
-             `\n${prefix}spam | JMLH | PESAN\`\`\``
-         ])
+         msg.channel.send(`\`\`\`js\nMau Spam Apa!.\n${prefix}spam | JMLH | PESAN\`\`\`\n`)
      } else
         //PMSPAM
      if (help === "pmspam") {
-         msg.channel.send([
-             '```js\nPM Spams Mantan!.' + 
-             `\n${prefix}pmspam | @MANTAN | JMLH | PESAN\`\`\``
-         ])
+         msg.channel.send(`\`\`\`js\nPM Spams Mantan!.\n${prefix}pmspam | @MANTAN | JMLH | PESAN\`\`\`\n`)
      } else
          //DSPAM
       if (help === "dspam") {
-          msg.channel.send([
-             '```js\nSpam Nembak Cewe abis ntu HApus!' + 
-             `\n${prefix}dspam | JMLH | TO SPAM\`\`\``
-          ]) 
+          msg.channel.send(`\`\`\`js\nSpam Nembak Cewe abis ntu HApus!\n${prefix}dspam | JMLH | TO SPAM\`\`\`\n`) 
       } else 
         //DPMSPAM
     if (help === "dpmspam") {
-        msg.channel.send([
-            '```js\nSpam DM Mantan SeKuy!!!' + 
-            `\n${prefix}dpmspam | @MANTAN | JMLH | PESAN\`\`\``
-        ])
+        msg.channel.send(`\`\`\`js\nSpam DM Mantan SeKuy!!!\n${prefix}dpmspam | @MANTAN | JMLH | PESAN\`\`\`\n`)
     } else
         //CHANNEL SPAM
     if (help === "cspam") {
-        msg.channel.send([
-            '```js\nSpam Kamar Mantan!' + 
-            `\n${prefix}cspam | #CHANNEL | JMLH | PESAN\`\`\``
-        ])
+        msg.channel.send(`\`\`\`js\nSpam Kamar Mantan!\n${prefix}cspam | #CHANNEL | JMLH | PESAN\`\`\`\n`)
     }
     }
 } else
@@ -172,7 +146,7 @@ msg.channel.send("Error, user not found.")
             console.log(err)}
     }
 });
-bot.login(process.env.TOKEN);
+bot.login(conf.token);
 
 bot.on("ready", async () => {
     console.log(`Logged in as : ${bot.user.tag}`);
